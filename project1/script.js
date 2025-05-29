6300500e7a75e13ea3a51f79dac725b7e9bd1288
Sangram Das <sangramraju143@gmail.com>
2025-05-29 12:13:14 +0530

    new two projects

---
+++ project1/script.js
@@ -0,0 +1,32 @@
+const score1 = document.getElementById ('id1');
+score1.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "red";
+});
+const score2 = document.getElementById ('id2');
+score2.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "blue";
+});
+const score3 = document.getElementById ('id3');
+score3.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "green";
+});
+const score4 = document.getElementById ('id4');
+score4.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "yellow";
+});
+const score5 = document.getElementById ('id5');
+score5.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "gray";
+});
+const score6 = document.getElementById ('id6');
+score6.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "black";
+});
+const score7 = document.getElementById ('id7');
+score7.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "white";
+});
+const score8 = document.getElementById ('id8');
+score8.addEventListener("click",function()  {
+     document.body.style.backgroundColor = "brown";
+});
---
+++ project2/script.js
@@ -0,0 +1,61 @@
+const taskList = document.getElementById('taskList');
+const taskinput = document.getElementById('taskinput');
+
+
+function loadTasks () {
+    const saved = localStorage.getItem('tasks');
+    if(saved){
+        taskList.innerHTML='';
+        JSON.parse(saved).forEach(task => createTaskElement(task.text,task.completed));
+
+    }
+
+
+}
+
+function saveTasks(){
+    const tasks = [];
+    document.querySelectorAll('#taskList li').forEach(li => {
+        tasks.push({
+            text:li.querySelector('span').textContent,
+            completed:li.classList.contains('completed')
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function createTaskElement (text ,completed = false){
+    const li = document.createElement("li");
+    const span = document.createElement('span');
+    span.textContent = text ;
+    span.onclick = () => {
+        li.classList.toggle('completed');
+        saveTasks();
+    };
+
+    const del = document.createElement('span');
+    del.textContent = "X";
+    del.className = 'delete';
+    del.onclick = () => {
+       li.remove();
+       saveTasks();
+    };
+
+    if(completed) li.classList.add('completed');
+    li.appendChild(span);
+    li.appendChild(del);
+    taskList.appendChild(li);
+}
+
+function addTask() {
+    const text = taskinput.value.trim();
+    if(text){
+        createTaskElement(text);
+        saveTasks();
+        taskinput.value ='';
+    }
+
+
+}
+
+window.onload = loadTasks;

49289e731e45303a1846d77708979b33ce8296fe
Sangram Das <sangramraju143@gmail.com>
2025-07-24 18:49:00 +0530

    In Details

---
+++ project19/main.js
@@ -0,0 +1,14 @@
+// 1. Ternary operator
+const isAdult = age >= 18 ? 'Yes' : 'No';
+
+// 2. Default greet parameter
+function greet( name = 'Guest'){
+    return `Hello ${name}`
+}
+
+// 3. Arrow Function
+const add = (a,b) => a+b;
+
+// 4. Destructuring Objects
+const [first , second] = person
+
